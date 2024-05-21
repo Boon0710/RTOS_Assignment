3b270373239bfde523940021bb202039249f1694
Boon0710 <minhhieutcd@gmail.com>
2024-05-21 19:27:56 +0700

    Initial commit

---
+++ Debug/Core/Src/assignment_irrigation.d
@@ -0,0 +1,6 @@
+Core/Src/assignment_irrigation.o: ../Core/Src/assignment_irrigation.c \
+ ../Core/Src/assignment_irrigation.h ../Core/Src/software_timer.h
+
+../Core/Src/assignment_irrigation.h:
+
+../Core/Src/software_timer.h:
